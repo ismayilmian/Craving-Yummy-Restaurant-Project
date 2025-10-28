621a9a11f3ebc32300f4e37304792d106e0c94c4
ismayilmian <ismayilmian30@gmail.com>
2025-10-28 18:43:01 +0530

    full Project Recent Update

--- menu.js
+++ menu.js
@@ -1,1 +1,20 @@
+var productcontainer = document.getElementById("product");
+var searchbar = document.getElementById("productSearch");
+var productlist = productcontainer.querySelectorAll("div");
+
+searchbar.addEventListener("keyup", function (event) {
+  var enteredtext = event.target.value.toUpperCase();
+
+  for (count = 0; count < productlist.length; count = count + 1) {
+    var productname = productlist[count].querySelector("p").textContent;
+    if (productname.toUpperCase().indexOf(enteredtext) < 0) {
+      productlist[count].style.display = "none";
+    }
+    else{
+        productlist[count].style.display = "block";
+
+    }
+    
+  }
+});
 
